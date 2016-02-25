75c0cc3f07dd64ffbdd0c3d1ccea2548597276c1
fediev <fediev@gmail.com>
2016-02-25 18:43:05 +0900

    Update: rename conn, pool to actor

--- test/Connection.test.js
+++ test/Connection.test.js
@@ -11,42 +11,42 @@ const Connection = require('../lib/Connection');
 const config = require('./config.json');
 
 describe('Connection', () => {
-  let conn;
+  let actor;
   beforeEach(() => {
-    conn = mysqler.createConnection(config);
+    actor = mysqler.createConnection(config);
   });
   afterEach(() => {
-    if (conn._actor.state !== 'disconnected') {
-      conn.end();
+    if (actor._actor.state !== 'disconnected') {
+      actor.end();
     }
   });
 
   describe('constructor', () => {
     it('should set actor with mysql Connection instance', () => {
-      conn.should.have.property('_actor');
-      conn.should.have.property('config');
-      return conn.query('SELECT 1+1 AS s1').should.become([{ s1: 2 }]);
+      actor.should.have.property('_actor');
+      actor.should.have.property('config');
+      return actor.query('SELECT 1+1 AS s1').should.become([{ s1: 2 }]);
     });
   });
 
   describe('end()', ()=> {
     it('should end connection', () => {
-      conn.end();
-      conn._actor.state.should.eql('disconnected');
+      actor.end();
+      actor._actor.state.should.eql('disconnected');
     });
   });
 
   describe('destroy()', ()=> {
     it('should destroy connection', () => {
-      conn.destroy();
-      conn._actor.state.should.eql('disconnected');
+      actor.destroy();
+      actor._actor.state.should.eql('disconnected');
     });
   });
 
   describe('query()', () => {
     it('should select data', () => {
       const sql = 'SELECT * FROM tbm_select';
-      return conn.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.should.have.lengthOf(6);
         const row = result[0];
@@ -55,21 +55,21 @@ describe('Connection', () => {
     });
     it('should update data and get affectedRows', () => {
       const sql = 'UPDATE tbm_update SET price = 10 WHERE color = "red"';
-      return conn.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.affectedRows.should.eql(2);
       });
     });
     it('should update data and get changedRows', () => {
       const sql = 'UPDATE tbm_update SET price = 20 WHERE color = "red"';
-      return conn.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.changedRows.should.eql(2);
       });
     });
     it('should insert data', () => {
       const sql = 'INSERT INTO tbm_insert_delete () VALUES (), ()';
-      return conn.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.affectedRows.should.eql(2);
         result.insertId.should.be.above(6);
@@ -77,11 +77,11 @@ describe('Connection', () => {
     });
     it('should delete data', () => {
       const sql1 = 'SELECT * FROM tbm_insert_delete';
-      return conn.query(sql1)
+      return actor.query(sql1)
       .then((result1) => {
         const sql2 = 'DELETE FROM tbm_insert_delete';
         const expected = result1.length;
-        return conn.query(sql2)
+        return actor.query(sql2)
         .then((result2) => {
           result2.affectedRows.should.eql(expected);
         });
@@ -89,67 +89,67 @@ describe('Connection', () => {
     });
     it('should set this.lastSql', () => {
       const sql = 'SELECT * FROM tbm_select';
-      return conn.query(sql)
+      return actor.query(sql)
       .then(() => {
-        conn.lastSql.should.eql(sql);
+        actor.lastSql.should.eql(sql);
       });
     });
     it('should set this.numRows on select', () => {
       const sql = 'SELECT * FROM tbm_select';
-      return conn.query(sql)
+      return actor.query(sql)
       .then((result) => {
         const expected = result.length;
-        conn.numRows.should.eql(expected);
-        should.not.exist(conn.affectedRows);
-        should.not.exist(conn.changedRows);
-        should.not.exist(conn.insertId);
+        actor.numRows.should.eql(expected);
+        should.not.exist(actor.affectedRows);
+        should.not.exist(actor.changedRows);
+        should.not.exist(actor.insertId);
       });
     });
     it('should set this.affectedRows on update', () => {
       const sql = 'UPDATE tbm_update SET price = 30 WHERE color = "red"';
-      return conn.query(sql)
+      return actor.query(sql)
       .then(() => {
         const expected = 2;
-        should.not.exist(conn.numRows);
-        conn.affectedRows.should.eql(expected);
-        conn.changedRows.should.be.at.least(0);
-        conn.insertId.should.eql(0);
+        should.not.exist(actor.numRows);
+        actor.affectedRows.should.eql(expected);
+        actor.changedRows.should.be.at.least(0);
+        actor.insertId.should.eql(0);
       });
     });
     it('should set this.changedRows on update', () => {
       const sql = 'UPDATE tbm_update SET price = 40 WHERE color = "red"';
-      return conn.query(sql)
+      return actor.query(sql)
       .then(() => {
         const expected = 2;
-        should.not.exist(conn.numRows);
-        conn.affectedRows.should.eql(expected);
-        conn.changedRows.should.eql(expected);
-        conn.insertId.should.eql(0);
+        should.not.exist(actor.numRows);
+        actor.affectedRows.should.eql(expected);
+        actor.changedRows.should.eql(expected);
+        actor.insertId.should.eql(0);
       });
     });
     it('should set this.affectedRows, insertId on insert', () => {
       const sql = 'INSERT INTO tbm_insert_delete () VALUES (), ()';
-      return conn.query(sql)
+      return actor.query(sql)
       .then(() => {
         const expected = 2;
-        should.not.exist(conn.numRows);
-        conn.affectedRows.should.eql(expected);
-        conn.changedRows.should.eql(0);
-        conn.insertId.should.be.above(6);
+        should.not.exist(actor.numRows);
+        actor.affectedRows.should.eql(expected);
+        actor.changedRows.should.eql(0);
+        actor.insertId.should.be.above(6);
       });
     });
     it('should set this.affectedRows on delete', () => {
       const sql1 = 'SELECT * FROM tbm_insert_delete';
-      return conn.query(sql1)
+      return actor.query(sql1)
       .then((result) => {
         const sql2 = 'DELETE FROM tbm_insert_delete';
         const expected = result.length;
-        return conn.query(sql2)
+        return actor.query(sql2)
         .then(() => {
-          should.not.exist(conn.numRows);
-          conn.affectedRows.should.eql(expected);
-          conn.changedRows.should.eql(0);
-          conn.insertId.should.eql(0);
+          should.not.exist(actor.numRows);
+          actor.affectedRows.should.eql(expected);
+          actor.changedRows.should.eql(0);
+          actor.insertId.should.eql(0);
         });
       });
     });
--- test/Pool.test.js
+++ test/Pool.test.js
@@ -11,35 +11,35 @@ const Pool = require('../lib/Pool');
 const config = require('./config.json');
 
 describe('Pool', () => {
-  let pool;
+  let actor;
   beforeEach(() => {
-    pool = mysqler.createPool(config);
+    actor = mysqler.createPool(config);
   });
   afterEach(() => {
-    if (!pool._actor._closed) {
-      pool.end();
+    if (!actor._actor._closed) {
+      actor.end();
     }
   });
 
   describe('constructor', () => {
     it('should set actor with mysql Pool instance', () => {
-      pool.should.have.property('_actor');
-      pool.should.have.property('config');
-      return pool.query('SELECT 1+1 AS s1').should.become([{ s1: 2 }]);
+      actor.should.have.property('_actor');
+      actor.should.have.property('config');
+      return actor.query('SELECT 1+1 AS s1').should.become([{ s1: 2 }]);
     });
   });
 
   describe('end()', () => {
     it('should end pool', () => {
-      pool.end();
-      pool._actor._closed.should.eql(true);
+      actor.end();
+      actor._actor._closed.should.eql(true);
     });
   });
 
   describe('query()', () => {
     it('should select data', () => {
       const sql = 'SELECT * FROM tbm_select';
-      return pool.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.should.have.lengthOf(6);
         const row = result[0];
@@ -48,21 +48,21 @@ describe('Pool', () => {
     });
     it('should update data and get affectedRows', () => {
       const sql = 'UPDATE tbm_update SET price = 10 WHERE color = "red"';
-      return pool.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.affectedRows.should.eql(2);
       });
     });
     it('should update data and get changedRows', () => {
       const sql = 'UPDATE tbm_update SET price = 20 WHERE color = "red"';
-      return pool.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.changedRows.should.eql(2);
       });
     });
     it('should insert data', () => {
       const sql = 'INSERT INTO tbm_insert_delete () VALUES (), ()';
-      return pool.query(sql)
+      return actor.query(sql)
       .then((result) => {
         result.affectedRows.should.eql(2);
         result.insertId.should.be.above(6);
@@ -70,11 +70,11 @@ describe('Pool', () => {
     });
     it('should delete data', () => {
       const sql1 = 'SELECT * FROM tbm_insert_delete';
-      return pool.query(sql1)
+      return actor.query(sql1)
       .then((result1) => {
         const sql2 = 'DELETE FROM tbm_insert_delete';
         const expected = result1.length;
-        return pool.query(sql2)
+        return actor.query(sql2)
         .then((result2) => {
           result2.affectedRows.should.eql(expected);
         });
@@ -82,67 +82,67 @@ describe('Pool', () => {
     });
     it('should set this.lastSql', () => {
       const sql = 'SELECT * FROM tbm_select';
-      return pool.query(sql)
+      return actor.query(sql)
       .then(() => {
-        pool.lastSql.should.eql(sql);
+        actor.lastSql.should.eql(sql);
       });
     });
     it('should set this.numRows on select', () => {
       const sql = 'SELECT * FROM tbm_select';
-      return pool.query(sql)
+      return actor.query(sql)
       .then((result) => {
         const expected = result.length;
-        pool.numRows.should.eql(expected);
-        should.not.exist(pool.affectedRows);
-        should.not.exist(pool.changedRows);
-        should.not.exist(pool.insertId);
+        actor.numRows.should.eql(expected);
+        should.not.exist(actor.affectedRows);
+        should.not.exist(actor.changedRows);
+        should.not.exist(actor.insertId);
       });
     });
     it('should set this.affectedRows on update', () => {
       const sql = 'UPDATE tbm_update SET price = 30 WHERE color = "red"';
-      return pool.query(sql)
+      return actor.query(sql)
       .then(() => {
         const expected = 2;
-        should.not.exist(pool.numRows);
-        pool.affectedRows.should.eql(expected);
-        pool.changedRows.should.be.at.least(0);
-        pool.insertId.should.eql(0);
+        should.not.exist(actor.numRows);
+        actor.affectedRows.should.eql(expected);
+        actor.changedRows.should.be.at.least(0);
+        actor.insertId.should.eql(0);
       });
     });
     it('should set this.changedRows on update', () => {
       const sql = 'UPDATE tbm_update SET price = 40 WHERE color = "red"';
-      return pool.query(sql)
+      return actor.query(sql)
       .then(() => {
         const expected = 2;
-        should.not.exist(pool.numRows);
-        pool.affectedRows.should.eql(expected);
-        pool.changedRows.should.eql(expected);
-        pool.insertId.should.eql(0);
+        should.not.exist(actor.numRows);
+        actor.affectedRows.should.eql(expected);
+        actor.changedRows.should.eql(expected);
+        actor.insertId.should.eql(0);
       });
     });
     it('should set this.affectedRows, insertId on insert', () => {
       const sql = 'INSERT INTO tbm_insert_delete () VALUES (), ()';
-      return pool.query(sql)
+      return actor.query(sql)
       .then(() => {
         const expected = 2;
-        should.not.exist(pool.numRows);
-        pool.affectedRows.should.eql(expected);
-        pool.changedRows.should.eql(0);
-        pool.insertId.should.be.above(6);
+        should.not.exist(actor.numRows);
+        actor.affectedRows.should.eql(expected);
+        actor.changedRows.should.eql(0);
+        actor.insertId.should.be.above(6);
       });
     });
     it('should set this.affectedRows on delete', () => {
       const sql1 = 'SELECT * FROM tbm_insert_delete';
-      return pool.query(sql1)
+      return actor.query(sql1)
       .then((result) => {
         const sql2 = 'DELETE FROM tbm_insert_delete';
         const expected = result.length;
-        return pool.query(sql2)
+        return actor.query(sql2)
         .then(() => {
-          should.not.exist(pool.numRows);
-          pool.affectedRows.should.eql(expected);
-          pool.changedRows.should.eql(0);
-          pool.insertId.should.eql(0);
+          should.not.exist(actor.numRows);
+          actor.affectedRows.should.eql(expected);
+          actor.changedRows.should.eql(0);
+          actor.insertId.should.eql(0);
         });
       });
     });
