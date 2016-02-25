79c625c1a51089ecd4a0c68add5ee86e79e38ef4
fediev <fediev@gmail.com>
2016-02-25 13:31:53 +0900

    Add: Pool constructor

--- test/Pool.js
+++ test/Pool.js
@@ -1,7 +1,28 @@
+/*eslint max-nested-callbacks: [2, 6] */
 'use strict';
 
-const should = require('chai').should();
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const should = chai.should();
+const mysqler = require('../index.js');
 const Pool = require('../lib/Pool');
 
+const config = require('./config.json');
+
 describe('Pool', () => {
+  describe('constructor', () => {
+    let pool;
+    before(() => {
+      pool = mysqler.createPool(config);
+    });
+    after(() => {
+      pool.end();
+    });
+    it('should set actor with mysql Pool instance', () => {
+      pool.should.have.property('actor');
+      pool.should.have.property('config');
+      return pool.query('SELECT 1+1 AS s1').should.become([{ s1: 2 }]);
+    });
+  });
 });
