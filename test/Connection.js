61f5f509d98b7d186ed9e78fb6237858305bdd25
fediev <fediev@gmail.com>
2016-02-25 11:34:24 +0900

    Add: bare class Mysqler, Connection, Pool

---
+++ test/Connection.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const should = require('chai').should();
+const Connection = require('../lib/Connection');
+
+describe('Connection', () => {
+});
---
+++ test/Pool.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const should = require('chai').should();
+const Pool = require('../lib/Pool');
+
+describe('Pool', () => {
+});
