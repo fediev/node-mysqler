5d1fe2302b9f3f5cf84fecc2709ae47c4f174433
fediev <fediev@gmail.com>
2016-02-25 13:26:19 +0900

    Add: Connection consturctor

--- test/Connection.js
+++ test/Connection.js
@@ -1,7 +1,28 @@
+/*eslint max-nested-callbacks: [2, 6] */
 'use strict';
 
-const should = require('chai').should();
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const should = chai.should();
+const mysqler = require('../index.js');
 const Connection = require('../lib/Connection');
 
+const config = require('./config.json');
+
 describe('Connection', () => {
+  describe('constructor', () => {
+    let conn;
+    before(() => {
+      conn = mysqler.createConnection(config);
+    });
+    after(() => {
+      conn.end();
+    });
+    it('should set actor with mysql Connection instance', () => {
+      conn.should.have.property('actor');
+      conn.should.have.property('config');
+      return conn.query('SELECT 1+1 AS s1').should.become([{ s1: 2 }]);
+    });
+  });
 });
