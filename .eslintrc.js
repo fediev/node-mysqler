711de2385bdc8a95a0b886ece6bd91d598234982
fediev <fediev@gmail.com>
2016-02-27 23:05:21 +0900

    Update: .eslintrc.js r.0.2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,33 @@
-/*eslint quote-props: 0, no-inline-comments: 0 */
-// TODO: check Stylistic Issues and ECMA 6
-// TODO: compare with https://github.com/eslint/eslint/blob/master/packages/eslint-config-eslint/default.yml
 /**
  * .eslintrc.js
- * base: .eslintrc.js r.0.2.0-pre (eslint v2.2.0)
- * modified: NO
+ * base: r.0.2.0
+ * eslint: ^2.2.0
+ * modified: -
  */
+
 module.exports = {
-  'root': true,
-  'env': {
-    'browser': true,
-    'node': true,
-    'commonjs': true,
-    'es6': true,
-    'mocha': true,
-    'jquery': true,
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+    mocha: true,
+    jquery: true,
   },
-  'parserOptions': {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'jsx': true,
-      'experimentalObjectRestSpread': true,
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
     },
   },
-  'plugins': [
+  plugins: [
     'react',
   ],
   'extends': 'eslint:recommended',
-  'rules': {
+  rules: {
     // Possible Errors
     'comma-dangle': [1, 'always-multiline'],  // override:recommended
     'no-cond-assign': [2, 'except-parens'],
@@ -57,9 +56,9 @@ module.exports = {
     'no-unreachable': 2,
     'use-isnan': 2,
     'valid-jsdoc': [1, {
-      'prefer': {
+      prefer: {
         'return': 'returns',
-        'requireReturn': 'true',
+        requireReturn: 'true',
       },
     }],
     'valid-typeof': 2,
@@ -80,7 +79,7 @@ module.exports = {
     'no-caller': 2,
     'no-case-declarations': 2,
     'no-div-regex': 2,
-    'no-else-return': 1,
+    'no-else-return': 0,
     'no-empty-function': 1,
     'no-empty-pattern': 2,
     'no-eq-null': 2,
@@ -99,11 +98,7 @@ module.exports = {
     'no-lone-blocks': 2,
     'no-loop-func': 2,
     'no-magic-numbers': 0,
-    'no-multi-spaces': [2, {
-      'exceptions': {
-        'Property': false,
-      },
-    }],
+    'no-multi-spaces': [2, { exceptions: { Property: false } }],
     'no-multi-str': 2,
     'no-native-reassign': 2,
     'no-new': 2,
@@ -148,7 +143,7 @@ module.exports = {
     'no-undef': 2,
     'no-undef-init': 2,
     'no-undefined': 2,
-    'no-unused-vars': [1, { 'vars': 'all', 'args': 'after-used' }], // override:recommended
+    'no-unused-vars': [1, { vars: 'all', args: 'after-used' }], // override:recommended
     'no-use-before-define': [2, 'nofunc'],
 
     // Node.js and CommJS
@@ -156,8 +151,8 @@ module.exports = {
     'global-require': 2,
     'handle-callback-err': [1, '^(err|error)$'],
     'no-mixed-requires': [2, {
-      'grouping': true,
-      'allowCall': true,
+      grouping: true,
+      allowCall: true,
     }],
     'no-new-require': 2,
     'no-path-concat': 2,
@@ -168,66 +163,122 @@ module.exports = {
 
     // Stylistic Issues
     'array-bracket-spacing': [2, 'never'],
-    'block-spacing': 2,
-    'brace-style': [2, '1tbs'],
-    'camelcase': [2, { 'properties': 'never' }],
-    'comma-spacing': 2,
+    'block-spacing': [2, 'always'],
+    'brace-style': [2, '1tbs', { allowSingleLine: true }],
+    'camelcase': [2, { properties: 'never' }],
+    'comma-spacing': [2, { before: false, after: true }],
     'comma-style': [2, 'last'],
     'computed-property-spacing': [2, 'never'],
-    'consistent-this': [2, 'self'],
+    'consistent-this': [2, 'self', 'context'],
     'eol-last': 2,
-    'func-names': 1, // intentional report level 1
-    // 'func-style': [2, 'declaration'], // change when needed
+    'func-names': 1,
+    'func-style': [0, 'expression', { allowArrowFunctions: true }],
+    'id-blacklist': 0,
+    'id-length': [1, { min: 2, exceptions: ['i', 'j', 'v', 'x', 'y'] }],
+    'id-match': [0],
+    'indent': [2, 2, {
+      SwitchCase: 1,
+      VariableDeclarator: { 'var': 2, 'let': 2, 'const': 3 },
+    }],
     'jsx-quotes': [2, 'prefer-double'],
-    'key-spacing': [2, { 'beforeColon': false, 'afterColon': true }],
+    'key-spacing': [2, {
+      beforeColon: false,
+      afterColon: true,
+      mode: 'strict',
+    }],
+    'keyword-spacing': 2,
+    'linebreak-style': [2, 'unix'],
+    'lines-around-comment': [2, { beforeBlockComment: true }],
+    'max-depth': [1, 4],
+    'max-len': [2, 80, 2, {
+      ignoreComments: true,
+      ignoreTrailingComments: true,
+      ignoreUrls: true,
+    }],
     'max-nested-callbacks': [2, 3],
+    'max-params': [1, 8],
+    'max-statements': [1, 20],
     'new-cap': 2,
     'new-parens': 2,
+    'newline-after-var': 0,
+    'newline-per-chained-call': [0, { ignoreChainWithDepth: 3 }],
     'no-array-constructor': 2,
+    'no-bitwise': 2,
+    'no-continue': 2,
     'no-inline-comments': 2,
     'no-lonely-if': 2,
     'no-mixed-spaces-and-tabs': 2,
-    'no-multiple-empty-lines': [2, { 'max': 2 }],
+    'no-multiple-empty-lines': [2, { max: 2, maxBOF: 0, maxEOF: 0 }],
     'no-negated-condition': 2,
     'no-nested-ternary': 2,
     'no-new-object': 2,
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'no-restricted-syntax': [2, 'WithStatement'],
     'no-spaced-func': 2,
+    'no-ternary': 0,
     'no-trailing-spaces': 2,
-    // 'no-underscore-dangle': 2, // change when needed
-    'no-unneeded-ternary': 2,
+    'no-underscore-dangle': [1, { allowAfterThis: true }],
+    'no-unneeded-ternary': [2, { defaultAssignment: false }],
+    'no-whitespace-before-property': 2,
     'object-curly-spacing': [2, 'always'],
     'one-var': [2, 'never'],
     'one-var-declaration-per-line': [2, 'always'],
+    'operator-assignment': [1, 'always'],
     'operator-linebreak': [2, 'before'],
     'padded-blocks': [2, 'never'],
-    'quote-props': [2, 'as-needed', { 'keywords': true }],
-    // 'require-jsdoc': 1, // change when needed
-    'semi-spacing': [2, { 'before': false, 'after': true }],
-    'keyword-spacing': 2,
+    'quote-props': [2, 'as-needed', { keywords: true, numbers: true }],
+    'quotes': [2, 'single', 'avoid-escape'],
+    'require-jsdoc': [1, {
+      require: {
+        FunctionDeclaration: true,
+        MethodDefinition: true,
+        ClassDeclaration: true,
+      },
+    }],
+    'semi': [2, 'always'],
+    'semi-spacing': [2, { before: false, after: true }],
+    'sort-imports': 0,
+    'sort-vars': 0,
     'space-before-blocks': [2, 'always'],
     'space-before-function-paren': [2, 'never'],
     'space-in-parens': [2, 'never'],
     'space-infix-ops': 2,
-    'space-unary-ops': [2, { 'words': true, 'nonwords': false }],
+    'space-unary-ops': [2, { words: true, nonwords: false }],
     'spaced-comment': [2, 'always', {
-      'exceptions': ['-', 'eslint'],
-      'markers': [
-        'global',
+      exceptions: ['-'],
+      markers: [
         'eslint',
-        'eslint-enable',
         'eslint-disable',
+        'eslint-enable',
         'eslint-env',
+        'global',
       ],
     }],
-    'indent': [2, 2, {
-      'SwitchCase': 1,
-      'VariableDeclarator': { 'var': 2, 'let': 2, 'const': 3 },
-    }],
-    'quotes': [2, 'single', 'avoid-escape'],
-    'linebreak-style': [2, 'unix'],
-    'semi': [2, 'always'],
-    'max-len': [2, 80, 2, { 'ignoreComments': true, 'ignoreUrls': true }],
+    'wrap-regex': 1,
 
     // ECMAScript 6
+    'arrow-body-style': [2, 'always'],
+    'arrow-parens': [2, 'always'],
+    'arrow-spacing': [2, { 'before': true, 'after': true }],
+    'constructor-super': 2,
+    'generator-star-spacing': [2, 'after'],
+    'no-class-assign': 2,
+    'no-confusing-arrow': 2,
+    'no-const-assign': 2,
+    'no-dupe-class-members': 2,
+    'no-new-symbol': 2,
+    'no-this-before-super': 2,
+    'no-useless-constructor': 2,
+    'no-var': 2,
+    'object-shorthand': [2, 'always'],
+    'prefer-arrow-callback': 1,
+    'prefer-const': 1,
+    'prefer-reflect': 0,
+    'prefer-rest-params': 1,
+    'prefer-spread': 1,
+    'prefer-template': 1,
+    'require-yield': 2,
+    'template-curly-spacing': [2, 'never'],
+    'yield-star-spacing': [2, 'after'],
   },
 };
