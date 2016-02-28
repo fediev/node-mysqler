0b5cd1dafad1b43772f08f1f34402b2f0d506dc6
fediev <fediev@gmail.com>
2016-02-28 12:28:26 +0900

    Update: change wrong test description

--- test/mysqler.test.js
+++ test/mysqler.test.js
@@ -5,26 +5,26 @@ const mysqler = require('../index.js');
 const Connection = require('../lib/Connection');
 const Pool = require('../lib/Pool');
 
-describe('mysql', () => {
+describe('mysqler', () => {
   describe('createConnection()', () => {
     it('should get Connection', () => {
       mysqler.createConnection({}).should.instanceOf(Connection);
     });
   });
   describe('createPool()', () => {
-    it('should get Connection', () => {
+    it('should get Pool', () => {
       mysqler.createPool({}).should.instanceOf(Pool);
     });
   });
   describe('escape()', () => {
-    it('should get Connection', () => {
+    it('should escape string as value', () => {
       const str = "a'b";
       const expected = "'a\\'b'";
       mysqler.escape(str).should.eql(expected);
     });
   });
   describe('escapeId()', () => {
-    it('should get Connection', () => {
+    it('should escape string as id', () => {
       const str = 'ab';
       const expected = '`ab`';
       mysqler.escapeId(str).should.eql(expected);
